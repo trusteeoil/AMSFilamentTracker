edf1d75d65fb6e99e06f137953c98f2e1568f4c4
trusteeoil <43849448+trusteeoil@users.noreply.github.com>
2026-05-22 09:34:12 -0400

    First upload

---
+++ Filament Tracker/sw.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const CACHE_VERSION = 'v1';
+const CACHE_NAME = `filament-tracker-${CACHE_VERSION}`;
+
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
